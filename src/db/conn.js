12416e19dc40fd1222204292ed3088def0861f6b
Ash_333 <akamat62@gmail.com>
2021-08-16 12:10:30 +0545

    dbConnection

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose');
 
-mongoose.connect("mongodb://localhost:27017/students",{
+mongoose.connect("mongodb+srv://ashish:45h15h@cluster0.ggf2s.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",{
     useCreateIndex:true,
     useNewUrlParser:true,
     useUnifiedTopology:true
